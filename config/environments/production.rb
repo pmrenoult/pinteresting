7d3957ad0ee45a64e76c6a87e7d7ef67979570c9
Pierre-Maxence Renoult <renoultpm@gmail.com>
2015-04-07 11:11:32 +0200

    Implemented Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  config.action_mailer.default_url_options = { host: 'http://omr-pinteresting-pmr.herokuapp.com', port: 3000 }
+
 end
