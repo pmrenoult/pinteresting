47d05983ec162a2aa975c30e04ee1f9cb1aaf9cf
Pierre-Maxence Renoult <renoultpm@gmail.com>
2015-05-12 23:22:01 +0200

    Add Amazon S3 to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,13 @@ Rails.application.configure do
   #Required for Heroku
   config.action_mailer.default_url_options = { host: 'http://omr-pinteresting-pmr.herokuapp.com', port: 3000 }
 
-  }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
 }
 
 end
